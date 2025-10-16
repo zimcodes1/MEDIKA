9040ca1fc18c33f57aac5fea9dd7dd43c075b83f
Azimeh <azimehobadiah1@gmail.com>
2025-10-17 00:14:21 +0100

    Added the  'Why you choose us section' (Not yet responsive)

--- medika/src/components/Body.tsx
+++ medika/src/components/Body.tsx
@@ -4,12 +4,32 @@ function Body(){
     return(
         <div className="w-full h-auto px-20 pt-10 bg-blue-50">
             <h1 className="text-3xl font-bold text-gray-950 mb-5 font-[Mont]">Our Consulting Specialists</h1>
+            {/*-------------ConsultingSpecialistCards Here-------------------*/}
             <div className="flex justify-between items-center w-full">
                 <ConsultingSpecialistCard name='Covid-19 Test' img='/images/germ.png' text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae rem cumque adipisci temporibus."></ConsultingSpecialistCard>
                 <ConsultingSpecialistCard name="Heart & Lungs" img="/images/lungs.png" text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae rem cumque adipisci temporibus."></ConsultingSpecialistCard>
                 <ConsultingSpecialistCard name="Supliments" img="/images/bowl.png" text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae rem cumque adipisci temporibus."></ConsultingSpecialistCard>
                 <ConsultingSpecialistCard name='Mental Health' img="/images/brain.png" text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae rem cumque adipisci temporibus."></ConsultingSpecialistCard>
             </div>
+            
+            {/*--------------Why you choose us Section----------*/}
+            
+            <div className="w-full h-[250px] mt-20 flex justify-between items-center mb-20">
+                <div className="overflow-hidden rounded-2xl h-full w-[50%]">
+                    <img src="/images/lab.jpg" alt="Us" className="w-full"/>
+                </div>
+                <div className="w-[50%] h-full flex flex-col justify-start px-10">
+                    <h2 className="text-gray-800 text-2xl font-[mont]">Why You Choose Us?</h2>
+                    <p className="text-gray-800 text-xs my-2"><i className="fa fa-check text-green-700"></i>  Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p className="text-gray-800 text-xs my-2"><i className="fa fa-check text-green-700"></i>  Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p className="text-gray-800 text-xs my-2"><i className="fa fa-check text-green-700"></i>  Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p className="text-gray-800 text-xs my-2"><i className="fa fa-check text-green-700"></i>  Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p className="text-gray-800 text-xs my-2"><i className="fa fa-check text-green-700"></i>  Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                    <button className="py-3 w-[150px] cursor-pointer rounded-2xl mt-2 text-sm text-blue-700">Learn More <i className="fa fa-arrow-right"></i></button>
+                </div>
+            </div>
+
         </div>
     )
 }
--- medika/src/components/micros/ConsultingSpecialistCard.tsx
+++ medika/src/components/micros/ConsultingSpecialistCard.tsx
@@ -4,7 +4,7 @@ function ConsultingSpecialistCard({img, name, text}: {img: string, name: string,
     return(
         <div className="w-[250px] border border-gray-300 h-[250px] bg-blue-50 rounded-2xl shadow-lg flex flex-col justify-center items-start p-5 hover:scale-105 transition-all duration-500">
             <img src={img} alt={name} className="w-[60px] h-[60px] p-1 rounded-full bg-blue-100"/>
-            <h2 className="text-gray-800 text-2xl font-[mont] mt-2">{name}</h2>
+            <h2 className="text-gray-800 text-xl font-[mont] mt-2">{name}</h2>
             <p className="text-xs text-gray-800">{text}</p>
         </div>
     )
