778aa61ce239b4943bb67d5e1297343339285376
Azimeh <azimehobadiah1@gmail.com>
2025-10-17 21:54:41 +0100

    Added responsiveness for the header and hero sections (Though no mobile navbar)

--- medika/src/components/TopBar.tsx
+++ medika/src/components/TopBar.tsx
@@ -4,15 +4,16 @@ const TopBar = ()=>{
     return(
         <div className='sticky top-0 bg-blue-100 h-[70px] w-full flex justify-between items-center px-20 max-sm:px-4 z-50'>
           <span><h3 className='max-sm:text-xl text-2xl font-bold font-[Mont] text-blue-700'>Medika</h3></span>
-          <span className='flex'>
-            <a href="#" className='max-sm:mx-1 max-sm:text-xs text-blue-950 font-semibold mx-2 hover:border-b-2 hover:border-blue-700 hover:text-blue-700 transition duration-600'>Home</a>
-            <a href="#" className='max-sm:mx-1 max-sm:text-xs text-blue-950 font-semibold mx-2 hover:border-b-2 hover:border-blue-700 hover:text-blue-700 transition duration-600'>About</a>
-            <a href="#" className='max-sm:mx-1 max-sm:text-xs text-blue-950 font-semibold mx-2 hover:border-b-2 hover:border-blue-700 hover:text-blue-700 transition duration-600'>Application</a>
-            <a href="#" className='max-sm:mx-1 max-sm:text-xs text-blue-950 font-semibold mx-2 hover:border-b-2 hover:border-blue-700 hover:text-blue-700 transition duration-600'>History</a>
+          <span className='flex max-sm:hidden'>
+            <a href="#" className=' text-blue-950 font-semibold mx-2 hover:border-b-2 hover:border-blue-700 hover:text-blue-700 transition duration-600'>Home</a>
+            <a href="#" className=' text-blue-950 font-semibold mx-2 hover:border-b-2 hover:border-blue-700 hover:text-blue-700 transition duration-600'>About</a>
+            <a href="#" className=' text-blue-950 font-semibold mx-2 hover:border-b-2 hover:border-blue-700 hover:text-blue-700 transition duration-600'>Application</a>
+            <a href="#" className=' text-blue-950 font-semibold mx-2 hover:border-b-2 hover:border-blue-700 hover:text-blue-700 transition duration-600'>History</a>
           </span>
-          <span className=''>
+          <span className='flex justify-center items-center'>
             <button className='max-sm:text-xs max-sm:px-2 max-sm:mx-0 max-sm:ml-1 transition-all duration-700 mx-2 px-6 py-2 border-2 border-blue-700 rounded-3xl text-blue-700 font-semibold hover:text-gray-50 hover:bg-blue-700'>Log in</button>
             <button className='max-sm:text-xs max-sm:px-2 max-sm:mx-0 max-sm:ml-1 transition-all duration-700 mx-2 px-6 py-2 bg-blue-700 rounded-3xl text-gray-50 font-semibold hover:bg-transparent border-2 border-blue-700 hover:text-blue-700'>Sign Up</button>
+            <i className="fa fa-bars text-3xl ml-2 text-blue-700 cursor-pointer"></i>
           </span>
         </div>
     )
--- medika/src/components/micros/ButBar.tsx
+++ medika/src/components/micros/ButBar.tsx
@@ -1,22 +1,22 @@
 
 const ButBar = ()=>{
     return(
-        <div className="w-full absolute bottom-0 h-[100px] bg-blue-700 flex left-0 py-5 px-20">
-            <div className="w-[60%] h-full flex">
-                <span className="flex h-full flex-col justify-center text-center mx-5">
-                    <h2 className="text-4xl font-bold text-blue-50">24/7</h2>
-                    <p className="text-blue-200">Online Support</p>
+        <div className="w-full absolute bottom-0 h-[100px] max-sm:h-[60px] bg-blue-700 flex left-0 py-5 px-20 max-sm:px-5">
+            <div className="flex w-[60%] max-sm:w-full max-sm:justify-evenly max-sm:items-center h-full">
+                <span className="flex h-full flex-col justify-center text-center mx-5 max-sm:mx-0">
+                    <h2 className="text-4xl font-bold text-blue-50 max-sm:text-xl">24/7</h2>
+                    <p className="text-blue-200 max-sm:text-sm">Online Support</p>
                 </span>
-                <span className="flex h-full flex-col justify-center border-x border-blue-300 px-10 mx-10">
-                    <h2 className="text-4xl font-bold text-blue-50">100+</h2>
-                    <p className="text-blue-200">Doctors</p>
+                <span className="flex h-full flex-col justify-center border-x border-blue-300 px-10 max-sm:px-5 mx-10 max-sm:mx-0">
+                    <h2 className="text-4xl font-bold text-blue-50 max-sm:text-xl">100+</h2>
+                    <p className="text-blue-200 max-sm:text-sm">Doctors</p>
                 </span>
-                <span className="flex h-full flex-col justify-center text-center mx-5">
-                    <h2 className="text-4xl font-bold text-blue-50">1M+</h2>
-                    <p className="text-blue-200">Active Patients</p>
+                <span className="flex h-full flex-col justify-center text-center mx-5 max-sm:mx-0">
+                    <h2 className="text-4xl font-bold text-blue-50 max-sm:text-xl">1M+</h2>
+                    <p className="text-blue-200 max-sm:text-sm">Active Patients</p>
                 </span>
             </div>
-            <div className="w-[40%] h-full"></div>
+            <div className="w-[40%] max-sm:hidden h-full"></div>
         </div>
     )
 }
